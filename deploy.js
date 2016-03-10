b3767f60242e512af0e917c2856ee0a426d1f176
barriber <borisber87@gmail.com>
2016-03-11 01:29:39 +0200

    production

--- deploy.js
+++ deploy.js
@@ -1,5 +1,6 @@
 console.log('=====enter deploy.js ==========')
 if (process.env.NODE_ENV === 'production') {
+    console.log('=====PRODUCTION MODE ==========')
     var child_process = require('child_process');
     child_process.exec("webpack -p --config webpack.production.config.js", function (error, stdout, stderr) {
         if (error !== null) {
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,15 +1,16 @@
 var Webpack = require('webpack');
 var path = require('path');
 var nodeModulesPath = path.resolve(__dirname, 'node_modules');
-var buildPath = path.resolve(__dirname, 'public', 'build');
+var buildPath = path.join(__dirname, 'public', 'build');
 var mainPath = path.resolve(__dirname, 'app', 'main.js');
+var srcPath = path.join(__dirname, 'app');
 
 
 var config = {
 
     // We change to normal source mapping
     devtool: 'source-map',
-    entry: mainPath,
+    entry: path.join(srcPath, 'main.js'),
     output: {
         path: buildPath,
         filename: 'bundle.js'
