613a9f4e7740fb199ddf816c70bfb6f81ab6287c
barriber <borisber87@gmail.com>
2016-04-29 13:37:06 +0300

    fix login

--- app/router.js
+++ app/router.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import {Router, Route, browserHistory} from 'react-router';
+import {Router, Route, browserHistory, IndexRoute} from 'react-router';
 import MainLayout from './components/MainLayout';
 import RoundFixtures from './components/RoundFixtures';
 import Login from './containers/Login';
 
 export default (
-  <Router history={browserHistory}>
-      <Route component={MainLayout}>
-          <Route path="/" component={RoundFixtures} />
-          <Route path="/login" component={Login} />
-          <Route path="/results" component={RoundFixtures} />
-      </Route>
-  </Router>
+    <Router history={browserHistory}>
+        <Route component={MainLayout}>
+            <Route path="/" component={RoundFixtures}/>
+            <Route path="login" component={Login}/>
+            <Route path="results" component={RoundFixtures}/>
+        </Route>
+    </Router>
 );
