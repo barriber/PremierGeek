89af7bfdef4bb04f413bb2026fa7b188ad197718
barriber <borisber87@gmail.com>
2016-04-29 14:03:27 +0300

    change to browserHistory

--- app/router.js
+++ app/router.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import {Router, Route, browserHistory, IndexRoute} from 'react-router';
+import {Router, Route, hashHistory, IndexRoute} from 'react-router';
 import MainLayout from './components/MainLayout';
 import RoundFixtures from './components/RoundFixtures';
 import Login from './containers/Login';
 
 export default (
-    <Router history={browserHistory}>
+    <Router history={hashHistory}>
         <Route component={MainLayout}>
             <Route path="/" component={RoundFixtures}/>
             <Route path="login" component={Login}/>
