a567785e343700b5ef847356ee202d970b4d9ece
barriber <borisber87@gmail.com>
2016-05-07 08:22:27 +0300

    minor

--- deploy.js
+++ deploy.js
@@ -1,7 +1,9 @@
+const webpackPath = path.join(__dirname, '..', 'client', 'webpack.config.js');
+
 if (process.env.NODE_ENV === 'production') {
     console.log('=====PRODUCTION MODE ==========')
     var child_process = require('child_process');
-    child_process.exec("webpack -p --config webpack.production.config.js", function (error, stdout, stderr) {
+    child_process.exec(`webpack -p --config  ${webpackPath}`, function (error, stdout, stderr) {
         if (error !== null) {
             console.log('exec error: ' + error);
         }
