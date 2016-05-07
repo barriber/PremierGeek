84bd3f27f9987ac3814b0fb5398a383b54ad05fb
barriber <borisber87@gmail.com>
2016-05-07 09:17:10 +0300

    hub task

--- gulpfile.js
+++ gulpfile.js
@@ -5,10 +5,13 @@ const hub = require('gulp-hub');
 const install = require('gulp-install');
 const gulpSequence = require('gulp-sequence');
 
-hub(['./client/gulpfile.js']);
+
+gulp.task('hub', function() {
+    hub(['./client/gulpfile.js']);
+});
 
 gulp.task('install:client', function () {
     return gulp.src('./client/package.json').pipe(install());
 });
 
-gulp.task('init:client', gulpSequence(['install:client'], ['webpack:build']));
+gulp.task('init:client', gulpSequence(['install:client'], ['hub'], ['webpack:build']));
