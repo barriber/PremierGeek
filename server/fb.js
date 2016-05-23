af016d6b57b1bc8551d4eeab2b39cc2658c79626
barriber <borisber87@gmail.com>
2016-05-23 12:38:57 +0300

    fix callbackUrl

--- server/fb.js
+++ server/fb.js
@@ -1,5 +1,5 @@
 module.exports = {
     'appID' : '1025583144188581',
     'appSecret' : '2b3325d84e74b1147aea166dc2111285',
-    'callbackUrl' : 'http://localhost:8888/auth/facebook/callback'
+    'callbackUrl' : '/auth/facebook/callback'
 }
