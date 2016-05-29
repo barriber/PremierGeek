589061dddc02da9cb8e049741c75677a7bcebffb
barriber <borisber87@gmail.com>
2016-05-30 01:20:28 +0300

    add logger

--- client/app/configureStore.js
+++ client/app/configureStore.js
@@ -1,9 +1,30 @@
 import {createStore, applyMiddleware } from 'redux';
 import rootReducer from './reducers/reducers';
+import createLogger from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
+import {Iterable} from 'Immutable'
 
+const logger = createLogger({
+    stateTransformer: (state) => {
+        let newState = {};
+
+        for (var i of Object.keys(state)) {
+            if (Iterable.isIterable(state[i])) {
+                newState[i] = state[i].toJS();
+            } else {
+                newState[i] = state[i];
+            }
+        };
+
+        return newState;
+    }
+});
+// const middlewares = [thunk];
+// if (process.env.NODE_ENV === `development`) {
+//     middlewares.push(logger);
+// }
 export default function configureStore() {
-    const store =  createStore(rootReducer, applyMiddleware(thunkMiddleware));
+    const store =  createStore(rootReducer, applyMiddleware(thunkMiddleware, logger));
 
     //if (module.hot) {
     //    // Enable Webpack hot module replacement for reducers
