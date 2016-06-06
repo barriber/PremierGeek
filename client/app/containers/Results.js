44d68120db3786bd6b073fff307b0b133f5aa955
barriber <borisber87@gmail.com>
2016-06-07 02:00:35 +0300

    small bug fix

--- client/app/containers/Results.js
+++ client/app/containers/Results.js
@@ -19,7 +19,7 @@ class Results extends Component {
             const orderedScores = orderBy(scores, ['points'], ['desc']);
             let position = 0;
             return (
-                <Col md={6} mdOffset={3} sm={10} smOffset={1} className="fixtures-section">
+                <Col md={6} mdOffset={3} sm={10} smOffset={1} className="scores-section">
                     <Row>
                         <Col md={2} mdOffset={10} sm={2} smOffset={10} className="text-center scores-title">
                             Scores
