6d6f710165eee463216362f1c063b0c085b9b1ba
barriber <borisber87@gmail.com>
2016-06-12 02:28:22 +0300

    add ongoing match calculation

--- client/app/components/PastResultsModal.js
+++ client/app/components/PastResultsModal.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import {Row, Col, Image, Modal} from 'react-bootstrap';
 import map from 'lodash/map';
+import orderBy from 'lodash/orderBy';
 
 export default function PastResultsModal({bets, showModal, closeModal, userName}) {
-    const teamLogo = function (src) {
+    const onGoingMatch = function (isEnded) {
         return (
-            <Col md={3}>
-                <Image src={src} responsive circle/>
+            <Col md={3} sm={2} className="on-going-match">
+                {!isEnded ? 'On Going' : null}
             </Col>
         );
     };
@@ -21,22 +22,23 @@ export default function PastResultsModal({bets, showModal, closeModal, userName}
     }
 
     const showUserResults = function () {
+        const orderedScores = orderBy(bets, ['startTime'], ['desc']);
         return (
             <div>
                 {
-                    map(bets, (bet) => {
+                    map(orderedScores, (bet) => {
                         return (
                             <Row className="historical-bet-row" key={bet.teams.homeTeamLogo}>
-                                <Col md={2} sm={4}>
+                                <Col md={2} sm={3}>
                                     <Image src={bet.teams.homeTeamLogo} responsive circle/>
                                 </Col>
                                 {betResult(bet.matchResult.homeScore, bet.bet.homeTeamScore)}
                                 {betResult(bet.matchResult.awayScore, bet.bet.awayTeamScore)}
-                                <Col md={2} sm={4}>
+                                <Col md={2} sm={3}>
                                     <Image src={bet.teams.awayTeamLogo} responsive circle/>
                                 </Col>
-
-                                <Col md={1} mdOffset={4} sm={2} className="text-center">
+                                {onGoingMatch(bet.isEnded)}
+                                <Col md={1} mdOffset={1} sm={2} className="text-center">
                                     {bet.points}
                                 </Col>
                             </Row>
