5513d23e4cd3da0969ca57ef9e0db6bbf82e16ac
barriber <borisber87@gmail.com>
2016-08-12 00:02:45 +0300

    remove winston add reqSerializer

--- server/bunyanLogger.js
+++ server/bunyanLogger.js
@@ -1,15 +1,26 @@
 const bunyan = require('bunyan');
 const path = require('path');
 const filePath = path.join(__dirname, '..', '..', 'logs', 'foo.log')
+function reqSerializer(req) {
+    return {
+        method: req.method,
+        url: req.url,
+        headers: req.headers,
+        body: req.body
+    };
+}
+
 const logger = bunyan.createLogger({
     name: 'logger',
+    serializers: {
+        req: reqSerializer
+    },
     streams: [{
         level: 'info',
         stream: process.stdout
     }, {
         level: 'info',
         path: filePath
-
     }]
 });
 
